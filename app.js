8c7fbd312a22fef9f7ab7bd230b934d1d4b25da5
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2013-08-06 11:34:59 -0300

    views + middlewares for front end

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const PORT = (process.env.PORT || 3000)
 		, VIEWS = __dirname + '/views'
 		, PUBLIC = __dirname + '/public'
+		, STYLUS = __dirname + '/stylus'
+		, MAXAGE = {maxAge: 60 * 60 * 1000}
+		, GZIP = {level: 9, memLevel: 9}
 
 var express = require('express')
 	, redis = require('./lib/redis_connect')()
 	, scraper = require('./lib/github_scraper')()
 	, agent = require('strong-agent')
+	, stylus = require('stylus')
+	, nib = require('nib')
+	, uglify = require('uglifyjs-middleware')
+	, site = require('./config.json')
 	, app = module.exports = express()
 ;
 
@@ -14,18 +21,31 @@ agent.profile(
   ['node-web-frameworks','Heroku']
 );
 
+app.use(express.favicon());
 app.use(express.logger('dev'));
 app.set('views', VIEWS);
 app.set('view engine', 'ejs');
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.compress({level: 9, memLevel: 9}));
 app.use(app.router);
-app.use(express.static(PUBLIC, {maxAge: 60 * 60 * 1000}));
+app.use(stylus.middleware({
+    src: STYLUS,
+    dest: PUBLIC,
+    compile: function (str, path, fn) {
+      return stylus(str)
+      	.set('filename', path)
+      	.set('compress', true)
+      	.use(nib());
+    }
+}));
+app.use(uglify(PUBLIC));
+app.use(express.static(PUBLIC, MAXAGE));
+app.use(express.compress(GZIP));
 
 app.get('/', function(req, res) {
 	//redis.zrange("nwf", 0, -1, function(err, result) {
-		res.end('<h1>Em desenvolvimento</h1>');
+		res.render('application', {site: site
+														 , domain: (req.protocol+'://'+req.host)});
 	//});
 });
 
