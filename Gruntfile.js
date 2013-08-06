ed309aa7e7da2d99c74371a4b4bb37482b91af86
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2013-08-06 19:40:33 -0300

    Organizando frontend

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+  
+  var config = {
+
+    // Concat ================================
+    concat: {
+      options: {
+        separator: ";"
+      },
+      tmp: {
+        src: [
+            "uglify/lib/jquery-*.js"
+          , "uglify/*.js"
+        ],
+        dest: "public/application.js"
+      }
+    },
+
+    // Minification ================================
+    uglify: {
+      minify: {
+        files: {
+          "public/application.js": ["public/application.js"]
+        }
+      }
+    },
+
+    // Stylus ================================
+    stylus: {
+      main: {
+        options: { compress: true },
+        files: { 'public/application.css': 'stylus/application.styl' }
+      }
+    }
+
+  };
+
+  grunt.initConfig(config);
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+
+  grunt.registerTask("default",["concat","uglify","stylus"]);
+};
--- app.js
+++ app.js
@@ -7,40 +7,21 @@ const PORT = (process.env.PORT || 3000)
 
 var express = require('express')
 	, redis = require('./lib/redis_connect')()
-	, scraper = require('./lib/github_scraper')()
-	, agent = require('strong-agent')
-	, stylus = require('stylus')
-	, nib = require('nib')
-	, uglify = require('uglifyjs-middleware')
+	, github_scraper = require('./lib/github_scraper')
 	, site = require('./config.json')
 	, app = module.exports = express()
 ;
 
-agent.profile(
-  process.env.NODEFLY_APPLICATION_KEY,
-  ['node-web-frameworks','Heroku']
-);
+require('./lib/cron_task')();
+require('./lib/nodefly_profiler')()
 
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.set('views', VIEWS);
 app.set('view engine', 'ejs');
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(stylus.middleware({
-    src: STYLUS,
-    dest: PUBLIC,
-    compile: function (str, path, fn) {
-      return stylus(str)
-      	.set('filename', path)
-      	.set('compress', true)
-      	.use(nib());
-    }
-}));
-app.use(uglify(PUBLIC));
-app.use(express.static(PUBLIC, MAXAGE));
 app.use(express.compress(GZIP));
+app.use(app.router);
+app.use(express.static(PUBLIC, MAXAGE));
 
 app.get('/', function(req, res) {
 	//redis.zrange("nwf", 0, -1, function(err, result) {
